340b5aa24d077d24873fa3c7731c6767aea89b5c
Adam Natrop <adamnatrop@gmail.com>
2021-05-08 20:02:43 -0500

    added arrows

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -12,7 +12,7 @@ function Table(props) {
                     <tr>
                     <th scope="col">Image</th>
                     {/* creates link on name field to sort alphabetically */}
-                    <th scope="col" className="sortByName" onClick={() => props.sortBy()} >Name <i className="fas fa-arrow-left"></i></th>
+                    <th scope="col" className="sortByName" onClick={() => props.sortBy()} >Name <i className="fas fa-arrows-alt-v"></i></th>
                     <th scope="col">Phone</th>
                     <th scope="col">Email</th>
                     <th scope="col">DOB</th>
